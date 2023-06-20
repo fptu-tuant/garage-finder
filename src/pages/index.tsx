8653db35bc079cf8acae747ed83885dbb95fc65b
Khanh <khanhquan.dev@gmail.com>
2023-06-21 00:18:42 +0700

    Edit Landing Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { Button, Cascader, Form, Input, Select, Typography } from 'antd';
-
 import { CarIllustrate } from '@/components';
 import { GARAGE_SERVICES, VIETNAM_PROVINCES } from '@/constants';
+import { Fragment } from 'react';
 
 export default function HomePage() {
   const locationCascaderOptions = VIETNAM_PROVINCES.map((province) => ({
@@ -14,13 +14,13 @@ export default function HomePage() {
   }));
 
   return (
-    <>
-      <section className="grid grid-cols-5 min-h-[500px]">
+    <div className='bg-[#FAF8FC]'>
+      <section className="grid grid-cols-5 min-h-[500px] ml-10">
         <div className="flex flex-col justify-center col-span-2">
-          <Typography.Title className="uppercase text-slate-800 w-4/5">
+          <Typography.Title className="uppercase text-slate-800 w-4/5 ml-10 mb-5">
             CÁCH NHANH CHÓNG VÀ DỄ DÀNG ĐỂ ĐẶT DỊCH VỤ chăm sóc xe hơi
           </Typography.Title>
-          <Typography.Text className="text-slate-600 w-4/5">
+          <Typography.Text className="text-slate-600 w-4/5 ml-10">
             Chào mừng bạn đến với Garage Finder ! Chúng tôi hiểu rằng chiếc xe
             của bạn là một phần quan trọng trong cuộc sống của bạn và chúng tôi
             ở đây để đảm bảo rằng nó vận hành trơn tru.
@@ -164,6 +164,6 @@ export default function HomePage() {
         </div>
       </section>          
 
-    </>
+    </div>
   );
 }
