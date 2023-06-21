34b8974dbc5a664c4c2e71fed6d8b64cbd12cddd
khanhquan <quan@gmail.com>
2023-06-21 23:58:59 +0700

    implement next-intl

--- @types/translate.d.ts
+++ @types/translate.d.ts
@@ -1,4 +1,6 @@
-// Use type safe message keys with `next-intl`
-type Messages = typeof import('@/translate/vi.json');
+import en from '@/translate/en.json';
+import vi from '@/translate/vi.json';
+
+type Messages = typeof en & typeof vi;
 
 type IntlMessages = Messages;
