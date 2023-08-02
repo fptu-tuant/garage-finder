7acf4e8c87891b410f1bbe0454cf9a8bc2053308
kiimvy <kimvy2901@gmail.com>
2023-08-02 20:59:48 +0700

    oke

--- src/pages/my-garages/manage/transaction.tsx
+++ src/pages/my-garages/manage/transaction.tsx
@@ -1,4 +1,4 @@
-import { Skeleton, Table, Typography } from 'antd';
+import { Button, Result, Skeleton, Table, Typography } from 'antd';
 import dayjs from 'dayjs';
 import { isEmpty } from 'lodash-es';
 import { useRouter } from 'next/router';
@@ -7,8 +7,11 @@ import { useAdminGetRegisteredSubscriptions } from '@/api/useAdminGetRegisteredS
 import { ManageGarageLayout } from '@/layouts';
 
 export default function ManageGarageTransactionPage() {
-  const { query } = useRouter();
-  const { garageId = '' } = query as { garageId: string };
+  const { query, push } = useRouter();
+  const { garageId = '', vnp_TransactionStatus } = query as {
+    garageId: string;
+    vnp_TransactionStatus: string;
+  };
 
   const { data, isLoading } = useAdminGetRegisteredSubscriptions({
     garageId: +garageId,
@@ -20,31 +23,52 @@ export default function ManageGarageTransactionPage() {
       <Typography.Title level={3}>Giao dịch</Typography.Title>
 
       <Skeleton active loading={isLoading}>
-        <Table
-          columns={[
-            { title: 'Tên gói', render: (_, item) => item.name },
-            { title: 'Thời hạn', render: (_, item) => item.period },
-            {
-              title: 'Giá',
-              render: (_, item) =>
-                Intl.NumberFormat('vi-VN', {
-                  style: 'currency',
-                  currency: 'VND',
-                }).format(item.price),
-            },
-            {
-              title: 'Ngày mua',
-              render: (_, item) => dayjs(item.dateCreate).format('DD-MM-YYYY'),
-            },
-            {
-              title: 'Ngày hết hạn',
-              render: (_, item) =>
-                dayjs(item.expirationDate).format('DD-MM-YYYY'),
-            },
-            { title: 'Trạng thái', render: (_, item) => 'API chưa có' },
-          ]}
-          dataSource={data}
-        />
+        {vnp_TransactionStatus ? (
+          <Result
+            status="success"
+            title="Mua gói thành viên thành công"
+            // subTitle="Order number: 2017182818828182881 Cloud server configuration takes 1-5 minutes, please wait."
+            extra={[
+              <Button
+                type="primary"
+                key="console"
+                onClick={() => {
+                  push({ query: { garageId } }, undefined, { shallow: true });
+                }}
+              >
+                Về trang giao dịch
+              </Button>,
+              // <Button key="buy">Buy Again</Button>,
+            ]}
+          />
+        ) : (
+          <Table
+            columns={[
+              { title: 'Tên gói', render: (_, item) => item.name },
+              { title: 'Thời hạn', render: (_, item) => item.period },
+              {
+                title: 'Giá',
+                render: (_, item) =>
+                  Intl.NumberFormat('vi-VN', {
+                    style: 'currency',
+                    currency: 'VND',
+                  }).format(item.price),
+              },
+              {
+                title: 'Ngày mua',
+                render: (_, item) =>
+                  dayjs(item.dateCreate).format('DD-MM-YYYY'),
+              },
+              {
+                title: 'Ngày hết hạn',
+                render: (_, item) =>
+                  dayjs(item.expirationDate).format('DD-MM-YYYY'),
+              },
+              { title: 'Trạng thái', render: (_, item) => 'API chưa có' },
+            ]}
+            dataSource={data}
+          />
+        )}
       </Skeleton>
     </div>
   );
