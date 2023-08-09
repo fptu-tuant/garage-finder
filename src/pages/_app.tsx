495403409e3627bc531de931dc21ceb1c2c8bb3a
Khanh Quan <khanhquan.dev@gmail.com>
2023-08-09 08:40:52 +0700

    delete button admin

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -23,6 +23,7 @@ export default function App({ Component, pageProps }: CustomAppProps) {
     <>
       <Head>
         <title>{title}</title>
+        
       </Head>
 
       <TranslateIntlProvider>
