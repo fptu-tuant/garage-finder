121ada1316ce840caa224fce94438ffbdce2f42b
kiimvy <kimvy2901@gmail.com>
2023-08-12 17:30:28 +0700

    okie

--- src/pages/my-garages/manage/transaction.tsx
+++ src/pages/my-garages/manage/transaction.tsx
@@ -1,6 +1,6 @@
 import { Button, Result, Skeleton, Table, Tag, Typography } from 'antd';
 import dayjs from 'dayjs';
-import { isNil } from 'lodash-es';
+import { get, isNil } from 'lodash-es';
 import { useRouter } from 'next/router';
 
 import { useAdminGetRegisteredSubscriptions } from '@/api/useAdminGetRegisteredSubscriptions';
@@ -10,6 +10,22 @@ const PAID_STATUS = {
   waiting: 'Đợi thanh toán',
   paid: 'Đã thanh toán',
   fail: 'Thanh toán không thành công',
+};
+
+const message = {
+  '00': 'Giao dịch thành công',
+  '07': 'Trừ tiền thành công. Giao dịch bị nghi ngờ (liên quan tới lừa đảo, giao dịch bất thường).',
+  '09': 'Giao dịch không thành công do: Thẻ/Tài khoản của khách hàng chưa đăng ký dịch vụ InternetBanking tại ngân hàng.',
+  '10': 'Giao dịch không thành công do: Khách hàng xác thực thông tin thẻ/tài khoản không đúng quá 3 lần',
+  '11': 'Giao dịch không thành công do: Đã hết hạn chờ thanh toán. Xin quý khách vui lòng thực hiện lại giao dịch.',
+  '12': 'Giao dịch không thành công do: Thẻ/Tài khoản của khách hàng bị khóa.',
+  '13': 'Giao dịch không thành công do Quý khách nhập sai mật khẩu xác thực giao dịch (OTP). Xin quý khách vui lòng thực hiện lại giao dịch.',
+  '24': 'Giao dịch không thành công do: Khách hàng hủy giao dịch',
+  '51': 'Giao dịch không thành công do: Tài khoản của quý khách không đủ số dư để thực hiện giao dịch.',
+  '65': 'Giao dịch không thành công do: Tài khoản của Quý khách đã vượt quá hạn mức giao dịch trong ngày.',
+  '75': 'Ngân hàng thanh toán đang bảo trì.',
+  '79': 'Giao dịch không thành công do: KH nhập sai mật khẩu thanh toán quá số lần quy định. Xin quý khách vui lòng thực hiện lại giao dịch',
+  '99': 'Các lỗi khác (lỗi còn lại, không có trong danh sách mã lỗi đã liệt kê)',
 };
 
 export default function ManageGarageTransactionPage() {
@@ -34,7 +50,7 @@ export default function ManageGarageTransactionPage() {
         {vnp_TransactionStatus ? (
           <Result
             status="success"
-            title="Mua gói thành viên thành công"
+            title={get(message, vnp_TransactionStatus)}
             extra={[
               <Button
                 type="primary"
